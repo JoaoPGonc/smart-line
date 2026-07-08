b87511d56fa284623eb672b4d43e77e912ca956a
danidavinasc <danidavinasc@gmail.com>
2026-07-08 19:59:31 -0300

    commit 2

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
     "revision": "80fb93accaf62078af9e9036b1fd45a4"
   }, {
     "url": "index.html",
-    "revision": "0.toonp0kjbe8"
+    "revision": "0.83lcnveucl8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
